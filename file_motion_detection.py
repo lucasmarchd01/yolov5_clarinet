c575cf3feefaa43d994b123e21e3f7b9a7911c0e
Lucas <lucas.march@mail.mcgill.ca>
2023-12-04 19:24:21 -0500

    heatmap, code formatting

--- file_motion_detection.py
+++ file_motion_detection.py
@@ -5,7 +5,7 @@ from natsort import natsorted
 import cv2
 import seaborn as sns
 import pandas as pd
-from sklearn.metrics import confusion_matrix
+from scipy.stats import chi2_contingency
 
 
 def read_clarinet_positions(results_directory):
@@ -234,6 +234,97 @@ def overlay_movement_direction(video_path, output_path, trajectory):
     print(f"Overlay video saved at: {output_path}")
 
 
+def visualization(clarinet_results_directory, person_results_directory):
+    clarinet_positions = read_clarinet_positions(clarinet_results_directory)
+    clarinet_bell_movement = get_clarinet_trajectory(clarinet_positions)
+
+    person_positions = read_person_positions(person_results_directory)
+
+    knee_bending_movement = get_bending_trajectory(person_positions)
+    forward_backward_movement = get_forward_backward_movement(person_positions)
+
+    # Extract frame numbers and movements
+    clarinet_frames, clarinet_movements = zip(*clarinet_bell_movement)
+    knee_frames, knee_movements = zip(*knee_bending_movement)
+    forward_backward_frames, forward_backward_movements = zip(
+        *forward_backward_movement
+    )
+
+    # Combine the movements into a single list for each frame
+    all_frames = set(clarinet_frames + knee_frames + forward_backward_frames)
+
+    # Create dictionaries to map frame numbers to movements
+    clarinet_mapping = dict(zip(clarinet_frames, clarinet_movements))
+    knee_mapping = dict(zip(knee_frames, knee_movements))
+    forward_backward_mapping = dict(
+        zip(forward_backward_frames, forward_backward_movements)
+    )
+
+    # Extract movements for each frame, defaulting to "No Movement" if not present
+    combined_movements = [
+        (
+            clarinet_mapping.get(frame, "No Movement"),
+            knee_mapping.get(frame, "No Movement"),
+            forward_backward_mapping.get(frame, "No Movement"),
+        )
+        for frame in all_frames
+    ]
+
+    # Extract individual movements for confusion matrix
+    clarinet_labels, knee_labels, forward_backward_labels = zip(*combined_movements)
+
+    # Specify the unique labels for each movement type
+    clarinet_unique_labels = ["Upward", "Downward", "No Movement"]
+    knee_unique_labels = ["Straightening", "Bending", "No Movement"]
+    forward_backward_unique_labels = ["Forward", "Backward", "No Movement"]
+
+    # Create confusion matrix
+    conf_matrix = np.zeros(
+        (
+            len(clarinet_unique_labels),
+            len(knee_unique_labels),
+            len(forward_backward_unique_labels),
+        )
+    )
+
+    # Populate the confusion matrix
+    for clarinet, knee, forward_backward in combined_movements:
+        clarinet_index = clarinet_unique_labels.index(clarinet)
+        knee_index = knee_unique_labels.index(knee)
+        forward_backward_index = forward_backward_unique_labels.index(forward_backward)
+        conf_matrix[clarinet_index, knee_index, forward_backward_index] += 1
+
+    # Create contingency table for chi-squared test
+    contingency_table = conf_matrix.sum(axis=2)
+
+    # Perform chi-squared test
+    chi2_stat, p_value, _, _ = chi2_contingency(contingency_table)
+
+    print(f"Chi-squared statistic: {chi2_stat}")
+    print(f"P-value: {p_value}")
+
+    # Plot 3D confusion matrix using a series of 2D heatmaps
+    fig = plt.figure(figsize=(15, 10))
+
+    for i in range(conf_matrix.shape[2]):
+        ax = fig.add_subplot(2, 2, i + 1)
+        sns.heatmap(
+            conf_matrix[:, :, i],
+            annot=True,
+            fmt=".0f",
+            cmap="Blues",
+            xticklabels=knee_unique_labels,
+            yticklabels=clarinet_unique_labels,
+            ax=ax,
+        )
+        ax.set_xlabel("Knee Movement")
+        ax.set_ylabel("Clarinet Movement")
+        ax.set_title(f"Forward/Backward Movement: {forward_backward_unique_labels[i]}")
+
+    plt.tight_layout()
+    plt.show()
+
+
 # Directory containing YOLOv5 detection results (.txt files)
 results_directory = (
     "/Users/lucasmarch/Projects/MUMT620_Project/yolov5/runs/detect/exp3/labels"
@@ -272,91 +363,12 @@ forward_backward_trajectory = get_forward_backward_movement(person_positions)
 # overlay_movement_direction(person_video_path, output_path, forward_backward_trajectory)
 
 
-# visualization
-
 clarinet_results_directory = (
-    "/Users/lucasmarch/Projects/MUMT620_Project/yolov5/runs/detect/exp2/labels"
+    "/Users/lucasmarch/Projects/MUMT620_Project/yolov5/runs/detect/exp4/labels"
 )
-clarinet_positions = read_clarinet_positions(clarinet_results_directory)
-clarinet_bell_movement = get_clarinet_trajectory(clarinet_positions)
 
 person_results_directory = (
-    "/Users/lucasmarch/Projects/MUMT620_Project/yolov5/runs/detect/exp5/labels"
-)
-person_positions = read_person_positions(person_results_directory)
-
-knee_bending_movement = get_bending_trajectory(person_positions)
-forward_backward_movement = get_forward_backward_movement(person_positions)
-
-
-# Extract frame numbers and movements
-clarinet_frames, clarinet_movements = zip(*clarinet_bell_movement)
-knee_frames, knee_movements = zip(*knee_bending_movement)
-forward_backward_frames, forward_backward_movements = zip(*forward_backward_movement)
-
-# Combine the movements into a single list for each frame
-all_frames = set(clarinet_frames + knee_frames + forward_backward_frames)
-
-# Create dictionaries to map frame numbers to movements
-clarinet_mapping = dict(zip(clarinet_frames, clarinet_movements))
-knee_mapping = dict(zip(knee_frames, knee_movements))
-forward_backward_mapping = dict(
-    zip(forward_backward_frames, forward_backward_movements)
+    "/Users/lucasmarch/Projects/MUMT620_Project/yolov5/runs/detect/exp7/labels"
 )
 
-# Extract movements for each frame, defaulting to "No Movement" if not present
-combined_movements = [
-    (
-        clarinet_mapping.get(frame, "No Movement"),
-        knee_mapping.get(frame, "No Movement"),
-        forward_backward_mapping.get(frame, "No Movement"),
-    )
-    for frame in all_frames
-]
-
-
-# Extract individual movements for confusion matrix
-clarinet_labels, knee_labels, forward_backward_labels = zip(*combined_movements)
-
-# Specify the unique labels for each movement type
-clarinet_unique_labels = ["Upward", "Downward", "No Movement"]
-knee_unique_labels = ["Straightening", "Bending", "No Movement"]
-forward_backward_unique_labels = ["Forward", "Backward", "No Movement"]
-
-# Create confusion matrix
-conf_matrix = np.zeros(
-    (
-        len(clarinet_unique_labels),
-        len(knee_unique_labels),
-        len(forward_backward_unique_labels),
-    )
-)
-
-# Populate the confusion matrix
-for clarinet, knee, forward_backward in combined_movements:
-    clarinet_index = clarinet_unique_labels.index(clarinet)
-    knee_index = knee_unique_labels.index(knee)
-    forward_backward_index = forward_backward_unique_labels.index(forward_backward)
-    conf_matrix[clarinet_index, knee_index, forward_backward_index] += 1
-
-
-# Plot 3D confusion matrix using a series of 2D heatmaps
-fig = plt.figure(figsize=(15, 10))
-
-for i in range(conf_matrix.shape[2]):
-    ax = fig.add_subplot(2, 2, i + 1)
-    sns.heatmap(
-        conf_matrix[:, :, i],
-        annot=True,
-        fmt=".0f",
-        cmap="Blues",
-        xticklabels=knee_unique_labels,
-        yticklabels=clarinet_unique_labels,
-        ax=ax,
-    )
-    ax.set_xlabel("Knee Movement")
-    ax.set_ylabel("Clarinet Movement")
-    ax.set_title(f"Forward/Backward Movement: {forward_backward_unique_labels[i]}")
-
-plt.tight_layout()
-plt.show()
+visualization(clarinet_results_directory, person_results_directory)
